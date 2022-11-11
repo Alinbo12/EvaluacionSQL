1b96d97539eb43796268d8d3c4e0db26969c65e5
AlinBurgos <alin.burgosolivero@gmail.com>
2022-11-11 19:02:25 -0300

    evaluacion

--- CasoDonJoseConsultas.sql
+++ CasoDonJoseConsultas.sql
@@ -1,3 +1,14 @@
 select * from minimarket.tipoproduct; 
 select * from minimarket.producto; 
+select * from minimarket.venta;
 
+ 
+select venta.fecha, venta.monto, venta.cantidadProd, venta.producto_id
+from minimarket.venta
+left join minimarket.producto on minimarket.venta.venta_id = minimarket.producto.producto_id;
+
+select producto.nombre, producto.precio, stock_id, stock.fecha, stock.cantidad, stock.proovedor_id, inversion.monto
+from minimarket.producto
+inner join minimarket.tipoProduct on minimarket.producto.producto_id = minimarket.tipoproduct.tipoProduct_id
+inner join minimarket.stock on minimarket.tipoproduct.tipoProduct_id = minimarket.stock.stock_id
+inner join minimarket.inversion on minimarket.producto.producto_id = minimarket.inversion.inversion_id
--- CasoDonJoseDML.sql
+++ CasoDonJoseDML.sql
@@ -9,10 +9,18 @@ insert into minimarket.producto (producto_id, nombre, precio, tipoProduct_id) va
 insert into minimarket.producto (producto_id, nombre, precio, tipoProduct_id) values (4, 'tallarines', 1500, 3);
 insert into minimarket.producto (producto_id, nombre, precio, tipoProduct_id) values (5, 'kilo manzana', 1000, 2);
 
-select * from minimarket.productos;
+select * from minimarket.stock;
 
 insert into minimarket.venta (venta_id, fecha, monto, cantidadProd, producto_id) values (1, '2019-11-11', 3000, 3, 5);
 insert into minimarket.venta (venta_id, fecha, monto, cantidadProd, producto_id) values (2, '2020-11-11', 10000, 5, 1);
-
+insert into minimarket.venta (fecha, monto, cantidadProd, producto_id) values ('2021-11-12', 10000, 10, 2);
 select * from minimarket.venta; 
 
+insert into minimarket.proovedor (nombre, rut, correo, direccion) 
+values ('incoludido', '76.585.348-6', 'contacto@incoludido.cl', 'Aldunate 1466');
+
+insert into minimarket.stock (cantidad, fecha, proovedor_id, tipoProduct_id)
+values(10, '2019-11-11', 1, 4);
+
+insert into minimarket.inversion (monto, fecha, cantidad, producto_id) 
+values (5000, '2019-11-11', 10, 4);
